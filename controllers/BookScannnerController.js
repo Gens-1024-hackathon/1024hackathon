710e3dabd49097f324152f43001a45070bc5834c
jiaoxiangyu <mingxiruliu@gmail.com>
2015-10-24 14:58:40 +0800

    view init

---
+++ controllers/BookScannnerController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jiaoxiangyu on 15/10/24.
+ */
+app.module('app').controller(BookScannerController,BookScannerController);
+
+//@Inject
+function BookScannerController(){
+
+    var myScope = this;
+
+    function initialize(){
+        //todo cleardata
+
+    }
+
+    //todo formcheck
+    this.checkData = function(){
+
+    };
+
+    //todo formcommit
+    this.saveBook = function () {
+
+    };
+
+    initialize();
+}
---
+++ controllers/EventScannerController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jiaoxiangyu on 15/10/24.
+ */
+app.module('app').controller(EventScannerController,EventScannerController);
+
+//@ngInject
+function EventScannerController(){
+
+    var myScope = this;
+
+    function initialize(){
+        //todo cleardata
+
+    }
+
+    //todo formcheck
+    this.dataCheck = function(){
+
+    };
+
+    //todo formcommit
+    this.commitEvent = function(){
+
+    };
+
+    initialize();
+}
